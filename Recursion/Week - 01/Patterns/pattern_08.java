e64b23dcf9ff802a952eca4e95b63451f0ace0f4
Satish-Kumar-Dev <satishkumarpoola18@gmail.com>
2025-12-08 20:49:19 +0530

    Changes made for Pattern_08

--- Recursion/Week - 01/Patterns/pattern_08.java
+++ Recursion/Week - 01/Patterns/pattern_08.java
@@ -12,11 +12,11 @@ class pattern_08{
     }
     static void pattern_08(int n){
         for(int row=0; row<n; row++){
-            int spaces = 2*n-1-row;
-            for(int space = row; space<spaces; space++){
+            int spaces = n - row;
+            for(int space = 0; space<spaces; space++){
                 System.out.print(" ");
             }
-            for (int col=0; col<=row; col++){
+            for (int col=1; col<2*row; col++){
                 System.out.print("*");
             }
             System.out.println();
